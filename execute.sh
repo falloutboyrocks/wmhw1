135ce8683659fbbe1c9116037dbee39d797f7c6d
b03902033 <b03902033@linux4.csie.ntu.edu.tw>
2017-03-29 13:42:15 +0800

    sf

--- execute.sh
+++ execute.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-python2 vsm.py -i queries/query-train.xml -o myrank.csv \
-              -m model -d NTCIR-dir 2 1.15 0.95
-python2 map.py queries/ans_train.csv myrank.csv
+touch log
+for i in 1.9 2 2.1
+do
+    for j in 1.1 1.15 1.2
+    do
+	for k in 1.0 1.05 1.1
+	do
+            python2 vsm.py -i queries/query-train.xml -o myrank \
+                           -m model -d NTCIR-dir $i $j $k
+	    echo 't = '$i 'q = ' $j 'k = ' $k >> log
+            python2 map.py queries/ans_train.csv myrank >> log
+        done
+    done
+done
